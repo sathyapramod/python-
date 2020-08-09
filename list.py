f9f6fed4ac274bef047a2234078c6de8769ca93c
Sathya pramod <sathyapramod12@gmail.com>
2020-08-09 19:12:41 +0530

    removed vscode

---
+++ list.py
@@ -0,0 +1,13 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# Accessing Elements in a List
+print(bicycles[0])
+print(bicycles[1].title())
+print(bicycles[-1])
+
+# Using Individual Values from a List
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+# Changing, adding, and removing elements
