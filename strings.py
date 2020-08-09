a5da39659a2dbf1726c5920f4addf0406fb9dd59
Sathya pramod <sathyapramod12@gmail.com>
2020-08-09 15:41:56 +0530

    Add files via upload
    String files added

---
+++ strings.py
@@ -0,0 +1,15 @@
+name = 'sathya pramod'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# string concatenating strings
+first_name = 'sathya'
+last_name = 'pramod'
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+# adding whitespace to string with tabs and newlines
+print("Languages:\n\t Python\n C \n Java")
